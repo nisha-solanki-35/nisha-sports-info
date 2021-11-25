e92888f6485f6530744937e2a78ff9de94c0493e
yudiz_nisha <nisha.s@itindia.co.in>
2021-11-25 22:51:04 +0530

    some css added

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,78 @@
 import axios from 'axios'
-import React from 'react'
-// import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap'
+import { Button } from 'reactstrap'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 
 export default function index ({ data }) {
-  const posts = data.data
+  const [start, setStart] = useState(0)
+  const [limit] = useState(4)
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    if (data) {
+      setArticles(data.data)
+    }
+  }, [data])
+
+  useEffect(() => {
+    start && getMoreArticles()
+  }, [start])
+
+  const getMoreArticles = async () => {
+    const response = await axios.post('https://backend.sports.info/api/v1/posts/recent',
+      { nStart: start, nLimit: limit, eSort: 'Latest', bRemoveBannerPosts: true })
+    const data = await response?.data
+
+    setArticles(data.data)
+  }
+
   return (<>
-      {posts.map((data, index) =>
-        <Card key={index}>
-          <CardImg
+      {articles.map((data, index) =>
+      <section key={index} className={styles.common_box}>
+        <div className='d-flex justify-content-start'>
+          <img
             alt='Card image cap'
             src={data.sImage}
-            top
-            width='100px'
+            className={styles.card_img}
+            // top
+            // width='100px'
           />
-          <CardBody>
-            <CardTitle tag="h5">
+          <div className={styles.text}>
+            <h4>
               <Link
                 href={`/${data._id}`}
                 as={`/articles/${data._id}`}
               >
                 {data.sTitle}
               </Link>
-            </CardTitle>
-            <CardText>
+            </h4>
               <div>
                 {data.sDescription}
               </div>
-              <div className='d-flex justify-content-between'>
-                <div className='d-inline-flex'>
-                  <p>{data.iId && data.iId.sFirstName ? data.iId.sFirstName : ''}</p>
-                  <p>{data.iId && data.iId.sLastName ? data.iId.sLastName : ''}</p>
-                </div>
+              <div className={`${styles.info} d-flex justify-content-between`}>
+                <p>{`${data.iId && data.iId.sFirstName ? data.iId.sFirstName : ''}  ${data.iId && data.iId.sLastName ? data.iId.sLastName : ''}`}</p>
                 <div className='d-inline-flex'>
                   <p>{data.nCommentsCount}</p>
                   <p>{data.nViewCounts}</p>
                 </div>
               </div>
-            </CardText>
-            <Button>
-              Button
-            </Button>
-          </CardBody>
-        </Card>)
+              </div>
+            </div>
+        </section>)
       }
+      <div className={styles.load_more_button}>
+        <Button color='danger' onClick={() => setStart(start + 4)}>Load More</Button>
+      </div>
     </>
   )
 }
 
 export const getStaticProps = async () => {
   const response = await axios.post('https://backend.sports.info/api/v1/posts/recent')
+  // { nStart: 0, nLimit: 4, eSort: 'Latest', bRemoveBannerPosts: true })
   const data = await response?.data
 
   return {
